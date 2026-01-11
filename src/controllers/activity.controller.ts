885e43010664d3784a691e071ec71186667bccec
owenunda <oweenunda.dev@gmail.com>
2026-01-11 01:57:54 -0500

    chore: implement getEvents controller and update routes to include event retrieval

--- src/controllers/activity.controller.ts
+++ src/controllers/activity.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { Activity } from "../models/activity.model";
 
+
 export const createActivity = async (req: Request, res: Response) => {
   try {
     const event = await Activity.create(req.body);
@@ -11,5 +12,48 @@ export const createActivity = async (req: Request, res: Response) => {
       success: false,
       message: "Invalid event data",
     });
+  }
+}
+
+export const getEvents = async (req: Request, res: Response) => {
+  try {
+    const {
+      type,
+      userId,
+      from,
+      to,
+      limit = '50'
+    } = req.query;
+
+    // query dinamica de para mongo
+    const query: any = {};
+    if (type) query.type = type;
+    if (userId) query.userId = userId;
+
+    if (from || to) {
+      query.createdAt = {};
+      if (from) {
+        const fromDate = new Date(from as string);
+        fromDate.setUTCHours(0, 0, 0, 0);
+        query.createdAt.$gte = fromDate;
+      }
+      if (to) {
+        const toDate = new Date(to as string);
+        toDate.setUTCHours(23, 59, 59, 999);
+        query.createdAt.$lte = toDate;
+      }
+    }
+
+    const events = await Activity.find(query)
+      .sort({ createdAt: -1 }) // el mas reciente primero // -1 -> DESC // 1 -> ASC
+      .limit(Number(limit))
+
+    res.json({
+      count: events.length,
+      events,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching events' })
   }
 }
